afac76399d8f5b03481a51a0376810614b635ee3
MadboyJames <Jkc36@pitt.edu>
2018-01-27 01:41:08 -0500

    Enemy Wave Fire Script

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    [SerializeField]
+    private float speed =0;
+    [SerializeField]
+   private float fireIntervel=100;
+    [SerializeField]
+    private float shotExpansionMultiplier=1;
+    [SerializeField]
+    private float shotDirectionDegrees=30;
+    [SerializeField]
+    private Vector2 shotSpeed= new Vector2(0,0);
+    [SerializeField]
+    private float shotKillTime = 100; 
+
+    private float counter=0;
+    public GameObject shotPrefab;
+    public Transform shotSpawn;
+   
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        Emission();
+    }
+
+    private void Move()
+    {
+        Vector2 playerPos = transform.position;
+        playerPos.x -= speed;
+        transform.position = playerPos;
+        
+    }
+
+    private void Emission()
+    {
+       if(counter<fireIntervel)
+        { counter += 1;
+        }else if (counter>=fireIntervel)
+        {
+            counter = 0;
+            Fire();
+        }
+
+    }
+    private void Fire()
+    {
+        float x = shotSpawn.transform.position.x;
+        float y = shotSpawn.transform.position.y;
+        float z = shotSpawn.transform.position.z; 
+        GameObject Shot = Instantiate(shotPrefab, new Vector3(x,y,z), new Quaternion(0,0,shotDirectionDegrees,0)) as GameObject;
+        ShotController shotContGetter = Shot.GetComponent<ShotController>();
+        shotContGetter.GetShotStats(shotSpeed,shotKillTime,shotExpansionMultiplier);
+        Vector3 rotA = Shot.transform.position;
+        rotA.z = shotDirectionDegrees;
+        Shot.transform.Rotate(rotA);
+       
+}
+   
+}
--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WaveController : MonoBehaviour {
     public float speed;
+    private float realSpeed;
+    public float currentDam;
+    public float recoverRate;
     // Use this for initialization
     void Start () {
        
@@ -11,10 +14,18 @@ public class WaveController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+        realSpeed = speed+currentDam;
         Vector2 playerPos = transform.position;
-        playerPos.x -= speed;
+        playerPos.x -= realSpeed;
         transform.position = playerPos;
-	}
+        Recovery();
+
+    }
+
+    void Recovery()
+    {
+        if(currentDam>0)
+        { currentDam -= recoverRate; }
+    }
 
 }
