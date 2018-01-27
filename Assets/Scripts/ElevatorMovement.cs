668918be00f7c4d20e047704364adbac87e80567
Tiddy <djb163@pitt.edu>
2018-01-27 13:07:11 -0500

    Elevator Animation

---
+++ Assets/Scripts/ElevatorMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorMovement : MonoBehaviour {
+    bool origin;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    //Variable to control if the elevator is "up" or "down" 
+
+    void Update()
+    {
+        
+    }
+    public void ActiveElevator()
+    {
+        if (origin)
+        {
+            animation["ElevatorMovement"].time = 0;
+            animation["Elevator"].speed = 1;
+            animation.Play("Elevator");
+            isAtOriginalPosition = false;
+        }
+        else
+        {
+            animation["Elevator"].time = animation["Elevator"].length;
+            animation["Elevator"].speed = -1;
+            animation.Play("Elevator");
+            isAtOriginalPosition = true;
+        }
+    }
+}
