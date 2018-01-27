ad7846b46b6fd22fdd9eca94923554f0414f814b
Tiddy <djb163@pitt.edu>
2018-01-27 00:58:52 -0500

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb2d;
     private Transform playerTransform;
-    public float moveY;
     private bool onGround = true;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float jumpVelocity;
     // Use this for initialization
+    private void Awake()
+    {
+    }
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         playerTransform = GetComponent<Transform>();
@@ -28,39 +33,22 @@ public class PlayerController : MonoBehaviour {
             playerPos.x -= .1f;
             playerTransform.position = playerPos;
         }
-        if (Input.GetKey("space"))
-        {
-            if (onGround)
-            {
-                moveY = .9f;
-                Jump();
-            }
-            else
-            {
 
-            }
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log(rb2d);
+            rb2d.velocity = Vector2.up * jumpVelocity;
+        } 
 
-        }
-        else
+        if (rb2d.velocity.y < 0)
         {
-            moveY = 0;
+            Debug.Log ("hit"); 
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-    }
-    void Jump()
-    {
-        //Jumping
-        //gameObject rb2b =
-        onGround = false;
-    }
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "Ground")
+        else if (rb2d.velocity.y > 0 && !Input.GetKey("space"))
         {
-            onGround = true;
-        }
-    }
-    private void FixedUpdate()
-    {
-       
+            Debug.Log("uh");
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }   
     }
 }
